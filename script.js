fbe43bc6a2cb521fa17ef2ddb1295f0a1d2214a8
Kari <kouglas2@gmail.com>
2022-09-23 14:30:04 -0500

    first commit
    this app functions by switching the theme from light to dark
    
    media queries, keyframes, classList

---
+++ script.js
@@ -0,0 +1,8 @@
+const body = document.getElementById('body')
+const toggle = document.getElementById('theme-toggle')
+
+toggle.addEventListener('click', toggleTheme)
+
+function toggleTheme() {
+    body.classList.toggle('dark');
+}
